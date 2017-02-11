9943e12c78f3e1b6f0f6826813bceb93a3c680b0
ObadaDarwish <obada_567@hotmail.co.uk>
2017-02-11 20:01:34 +0200

    finalizing new post and new product

--- src/app/admin-panel/new-post/new-post.component.ts
+++ src/app/admin-panel/new-post/new-post.component.ts
@@ -15,13 +15,13 @@ export class NewPostComponent implements OnInit {
   image: any;
   fileList: FileList;
 
-  constructor(private notify:NotificationsService,private loading:SlimLoadingBarService,private http: Http, private fb: FormBuilder, private admin: AdminPanelService) {
+  constructor(private notify: NotificationsService, private loading: SlimLoadingBarService, private http: Http, private fb: FormBuilder, private admin: AdminPanelService) {
     this.Admin = fb.group({
       video: [''],
       title: [''],
       description: ['']
     });
-    this.fileList=null;
+    this.fileList = null;
   }
 
   ngOnInit() {
@@ -34,41 +34,46 @@ export class NewPostComponent implements OnInit {
 
 
   postNewPost(form) {
-    this.loading.start(()=>{
-      console.log('loading');
-    });
-    let formData: FormData = new FormData();
+    if (form.title == '') {
+      this.notify.error('Error','No title found');
+    }
+    else {
+      this.loading.start(()=> {
+        console.log('loading');
+      });
+      let formData: FormData = new FormData();
 
-    if (this.fileList) {
-      if (this.fileList.length > 0) {
+      if (this.fileList) {
+        if (this.fileList.length > 0) {
 
-        for (var x = 0; x < this.fileList.length; x++) {
-          formData.append('images[]', this.fileList[x], this.fileList[x].name);
+          for (var x = 0; x < this.fileList.length; x++) {
+            formData.append('images[]', this.fileList[x], this.fileList[x].name);
+          }
         }
       }
+      formData.append('title', form.title);
+      formData.append('video', form.video);
+      formData.append('description', form.description);
+
+      let headers = new Headers();
+      let options = new RequestOptions({headers: headers});
+      this.http.post(`http://localhost:8000/newPost`, formData, options)
+        .map(res => res)
+        .catch(error => Observable.throw(error))
+        .subscribe(
+          data => {
+            console.log(data)
+            this.loading.complete();
+            this.notify.success('Success', 'Posted successfully');
+          },
+          error => {
+            console.log(error)
+            this.loading.complete();
+            this.notify.error('Error', 'Post can not be submitted');
+          }
+        )
+
+
     }
-    formData.append('title', form.title);
-    formData.append('video', form.video);
-    formData.append('description', form.description);
-
-    let headers = new Headers();
-    let options = new RequestOptions({headers: headers});
-    this.http.post(`http://localhost:8000/fileupload`, formData, options)
-      .map(res => res)
-      .catch(error => Observable.throw(error))
-      .subscribe(
-        data => {console.log(data)
-        this.loading.complete();
-        this.notify.success('Success','Posted successfully');
-        },
-        error => {
-          console.log(error)
-       this.loading.complete();
-          this.notify.error('Error','Post can not be submitted');
-        }
-      )
-
-
   }
-
 }
--- src/app/admin-panel/new-product/new-product.component.ts
+++ src/app/admin-panel/new-product/new-product.component.ts
@@ -1,4 +1,10 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {FormGroup, FormControl, FormBuilder, Validators} from '@angular/forms';
+import {AdminPanelService} from '../admin-panel.service';
+import {Http, Headers, RequestOptions} from '@angular/http';
+import {Observable} from 'rxjs';
+import {SlimLoadingBarService} from 'ng2-slim-loading-bar';
+import {NotificationsService} from 'angular2-notifications/src/notifications.service';
 
 @Component({
   selector: 'app-new-product',
@@ -6,10 +12,61 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./new-product.component.css']
 })
 export class NewProductComponent implements OnInit {
+  public Admin: FormGroup;
+  fileList: FileList;
+  constructor(private notify: NotificationsService, private loading: SlimLoadingBarService, private http: Http, private fb: FormBuilder, private admin: AdminPanelService) {
+    this.Admin = fb.group({
+      Name: [''],
+    });
+  }
 
-  constructor() { }
+  fileChange(event) {
+    this.fileList = event.target.files;
+    console.log(this.fileList);
+  }
 
   ngOnInit() {
   }
 
+  postNewProduct(form) {
+
+    if (form.Name==''||!this.fileList) {
+      this.notify.error('Error','Name or image not found');
+    }
+    else {
+      this.loading.start(()=> {
+        console.log('loading');
+      });
+      let formData: FormData = new FormData();
+
+      if (this.fileList) {
+        if (this.fileList.length > 0) {
+
+          for (var x = 0; x < this.fileList.length; x++) {
+            formData.append('images[]', this.fileList[x], this.fileList[x].name);
+          }
+        }
+      }
+      formData.append('name', form.Name);
+
+
+      let headers = new Headers();
+      let options = new RequestOptions({headers: headers});
+      this.http.post(`http://localhost:8000/newProduct`, formData, options)
+        .map(res => res)
+        .catch(error => Observable.throw(error))
+        .subscribe(
+          data => {
+            console.log(data);
+            this.loading.complete();
+            this.notify.success('Success', 'Posted successfully');
+          },
+          error => {
+            console.log(error);
+            this.loading.complete();
+            this.notify.error('Error', 'Post can not be submitted');
+          }
+        )
+    }
+  }
 }
