431e5602fcb248dc6e029244e68442d405af57c2
ObadaDarwish <obada_567@hotmail.co.uk>
2017-10-21 16:56:54 +0200

    exporting form

--- src/app/exporting/exporting.component.ts
+++ src/app/exporting/exporting.component.ts
@@ -2,26 +2,29 @@ import {Component, OnInit} from '@angular/core';
 import {SlimLoadingBarService} from 'ng2-slim-loading-bar';
 import {NotificationsService} from 'angular2-notifications/src/simple-notifications/services/notifications.service';
 import {ExportingService} from './exporting.service';
-
+import {FormGroup, FormBuilder, Validators}from '@angular/forms';
 @Component({
   selector: 'app-exporting',
   templateUrl: './exporting.component.html',
   styleUrls: ['./exporting.component.css']
 })
 export class ExportingComponent implements OnInit {
+  exportingform: FormGroup;
+  products: any;
 
-  constructor(private loading: SlimLoadingBarService, private notify: NotificationsService, private exporting: ExportingService) {
+  constructor(private fb: FormBuilder, private loading: SlimLoadingBarService, private notify: NotificationsService, private exporting: ExportingService) {
+    this.exportingform = fb.group({
+      'name': ['', Validators.required],
+      'email': ['', Validators.compose([Validators.required, Validators.pattern('^[a-zA-Z0-9]+(\.[_a-zA-Z0-9]+)*@[a-zA-Z0-9-]+(\.[a-z0-9-]+)*(\.[a-z]{2,15}( )*?)$')])],
+      'product': ['', Validators.required],
+      'quantity': ['', Validators.required],
+      'details': [''],
+      'date': ['', Validators.required]
+    })
   }
 
-  date: any;
-  name: any;
-  email: any;
-  product: any;
-  quantity: any;
-  details: any;
   minDate = new Date();
-  products: any;
-  ExtraDay: any;
+  isloading: boolean = false;
 
   ngOnInit() {
     this.exporting.getProducts().subscribe(
@@ -34,34 +37,30 @@ export class ExportingComponent implements OnInit {
     );
   }
 
-  ExpoertRequest() {
+  ExportRequest(value, valid) {
+    this.isloading = true;
     this.loading.start(()=> {
       console.log('Loading');
     });
-    this.ExtraDay = this.date.getFullYear() + '-' + (this.date.getMonth() + 1) + '-' + this.date.getDate();
-    if (this.date && this.name && this.email && this.product && this.quantity && this.details) {
-      this.exporting.postExportingRequest(this.name, this.email, this.product, this.ExtraDay, this.details, this.quantity, this.minDate).subscribe(
+    if (valid) {
+      this.exporting.postExportingRequest(this.exportingform.value.name, this.exportingform.value.email, this.exportingform.value.product,
+        this.exportingform.value.date, this.exportingform.value.details, this.exportingform.value.quantity,
+        this.minDate).subscribe(
         response=> {
           this.loading.complete();
           this.notify.success('Success', 'Exporting request has been sent');
-          console.log(response);
-          this.name = "";
-          this.email = "";
-          this.product = "";
-          this.date = "";
-          this.details = "";
-          this.quantity = "";
-
+          this.isloading = false;
+          this.exportingform.reset();
         },
         error=> {
           this.loading.complete();
           this.notify.error('Error', 'Can not send request');
-          console.log(error);
+          this.isloading = false;
         }
       );
     }
     else {
-      this.notify.error('Error', 'All fields are required');
+      this.notify.error('Error', '* fields are required');
     }
   }
 
